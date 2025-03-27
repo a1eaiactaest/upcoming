e358483be13e42b31e907d4f1678f5062563947c
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-03-27 17:57:19 +0100

    menu members

--- upcoming/AppDelegate.swift
+++ upcoming/AppDelegate.swift
@@ -8,25 +8,58 @@
 
 import Foundation
 import AppKit
+import EventKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
-
     var statusBarItem: NSStatusItem?
+    var eventStore = EKEventStore()
+    var timer: Timer?
     
     // swiftlint: disable line_length
     func applicationDidFinishLaunching(_ notification: Notification) {
         // main loop i guess
-        statusBarItem = NSStatusBar.system.statusItem(withLength:
-                                                        NSStatusItem.variableLength)
-        if let button = statusBarItem?.button {
-            button.action = #selector(statusBarButtonClicked(_:))
-            button.target = self
-            button.title = "open window"
-        }
+        setupMenuBar()
+        requestCalendarAccess()
+        startTimer()
     }
     
+    func setupMenuBar() {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let button = statusBarItem?.button {
+            button.title = "Loading.."
+        }
+        
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(
+            title: "Loading",
+            action: nil,
+            keyEquivalent: "")
+        )
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(
+            title: "Preferences",
+            action: #selector(openPreferences),
+            keyEquivalent: ",")
+        )
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(
+            title: "Quit",
+            action: #selector(NSApplication.terminate(_:)),
+            keyEquivalent: "q"
+        ))
+        
+        statusBarItem?.menu = menu
+
+    }
+    
+    func requestCalendarAccess(){}
+    
+    func startTimer(){}
+    
     @objc func statusBarButtonClicked(_ sender : NSStatusBarButton) {
-        print("menu bar item clicked")
+        print("menu bar item clicked edit")
         // TODO: handling logic here
     }
 
