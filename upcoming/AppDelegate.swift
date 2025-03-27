2e76953773d7f2ca68f6e8047c36628a2ec87697
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-03-27 12:57:56 +0100

    create and integrate appdelegate

---
+++ upcoming/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  upcoming
+//
+//  Created by Piotrek Rybiec on 27/03/2025.
+//
+
+
+import Foundation
+import AppKit
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    // swiftlint: disable line_length
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // main loop i guess
+    }
+
+    private func setupMyApp() {
+        // TODO: Add any intialization steps here.
+        print("Application started up!")
+    }
+}
