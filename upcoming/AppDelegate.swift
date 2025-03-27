44da20dbf5ba6d6e241ae64b956c47e43495b21d
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-03-27 13:08:59 +0100

    make app show something in menubar

--- upcoming/AppDelegate.swift
+++ upcoming/AppDelegate.swift
@@ -11,9 +11,23 @@ import AppKit
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var statusBarItem: NSStatusItem?
+    
     // swiftlint: disable line_length
     func applicationDidFinishLaunching(_ notification: Notification) {
         // main loop i guess
+        statusBarItem = NSStatusBar.system.statusItem(withLength:
+                                                        NSStatusItem.variableLength)
+        if let button = statusBarItem?.button {
+            button.action = #selector(statusBarButtonClicked(_:))
+            button.target = self
+            button.title = "open window"
+        }
+    }
+    
+    @objc func statusBarButtonClicked(_ sender : NSStatusBarButton) {
+        print("menu bar item clicked")
+        // TODO: handling logic here
     }
 
     private func setupMyApp() {
