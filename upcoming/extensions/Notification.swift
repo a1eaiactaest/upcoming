531a7df863754f504f24c0a4839fa935f9c31688
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-03-29 19:30:27 +0100

    embed extensions\

---
+++ upcoming/extensions/Notification.swift
@@ -0,0 +1,13 @@
+//
+//  Notification.swift
+//  upcoming
+//
+//  Created by Piotrek Rybiec on 29/03/2025.
+//
+
+import EventKit
+
+extension Notification.Name {
+    static let calendarDataDidChange = Notification.Name("calendarDataDidChange")
+}
+
