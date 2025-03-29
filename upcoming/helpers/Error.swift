8eb457e0322127eb9c4a556c2dbe261bf371e52d
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-03-29 12:58:45 +0100

    error helper

---
+++ upcoming/helpers/Error.swift
@@ -0,0 +1,17 @@
+//
+//  Error.swift
+//  upcoming
+//
+//  Created by Piotrek Rybiec on 29/03/2025.
+//
+
+import SwiftUI
+
+// TODO: add "report to devs" button
+func showErrorAlert(title: String, message: String) {
+    let alert = NSAlert()
+    alert.messageText = title
+    alert.informativeText = message
+    alert.addButton(withTitle: "OK")
+    alert.runModal()
+}
